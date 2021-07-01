e6496fd602448a821e8ce64704ad5be8c884fc99
Michael Fares <fares_michael@live.com>
2021-07-01 03:34:17 +0200

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('signup');
 });
+
+Route::get('/welcome', function (){
+   return view('welcome');
+});
+
+Route::post('/register', 'App\Http\Controllers\RegistrationController@store');
