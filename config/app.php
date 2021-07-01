69311c6f7b03d45fb8423cfb34bf1e9e24ad4c89
Michael Fares <fares_michael@live.com>
2021-07-01 14:01:08 +0200

    app changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'HelloTreeTest'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://hellotreetest.devs/'),
 
     'asset_url' => env('ASSET_URL', null),
 
